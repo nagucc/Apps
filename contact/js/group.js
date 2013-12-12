21b64343a113a8636611e794c72d38402e57f961
na57 <na57@qq.com>
2013-12-12 11:38:02 +0800

    调整首页显示内容及contact页固定信息显示逻辑

--- contact/js/group.js
+++ contact/js/group.js
@@ -1,4 +1,4 @@
-﻿var jQT, curGroup, wxmId;
+﻿var jQT, curGroup, wxmId, curContact;
 
 // 指示是否隐藏公共联系人
 var bHidePublic = true;
@@ -9,15 +9,17 @@ var gKey;
 // 描述联系人主从关系的表：
 var contactsMS = new Array();
 
+var dtdMe;
+
+jQuery.fn.center = function () {
+    this.css("position", "absolute");
+    this.css("top", ($(window).height() - this.height()) / 2 + $(window).scrollTop() + "px");
+    this.css("left", ($(window).width() - this.width()) / 2 + $(window).scrollLeft() + "px");
+    return this;
+}
+
+
 $(function () {
-    Nagu.Contact = {
-        ContactClass: '5d9a1f44-ab0e-47e0-bb92-ba8b47f5aa42',
-        Mobile: 'bd640461-41d2-4009-862e-0ebcd2d77b2c',
-        Email: '653a0707-2e03-4e93-937f-6168ddcc6723',
-        OfficePhone: '282c7749-1d94-41d2-afa0-45ca4e61b87d',
-        BelongsTo: '1ea23591-6d15-4dfb-b32d-3314f60a0a0b',
-        SubGroup: 'd5061aa6-c58d-4369-a8c0-9b7fa8825e67'
-    };
 
     jQT = new $.jQTouch({
         icon: 'jqtouch.png',
@@ -25,17 +27,28 @@ $(function () {
         addGlossToIcon: false,
         startupScreen: 'jqt_startup.png',
         statusBar: 'black-translucent',
-        preloadImages: []
+        preloadImages: ['images/startup.png']
     });
 
-    curGroup = getRequest()['id'];
+    // 显示启动页面
+    $('#startup').center();
+
+    curContact = getRequest()['id'];
     wxmId = getRequest()['wxm'];
     gKey = getRequest()['gkey'];
-    if (curGroup !== undefined && curGroup != null & curGroup != '') {
-        showGroup(curGroup);
+
+    dtdMe = Nagu.MM.getMe();
+
+    // 如果指定了ContactId，则直接显示Contact
+    if (curContact) {
+        //showGroup(curGroup);
+        showContact(curContact);
+    } else {
+        // 如果没有指定，则显示用户收藏（BelongTo）的联系人
+        showContactList();
     }
 
-    Nagu.MM.getMe().done(function (me) {
+    $.when(dtdMe).then(function (me) {
         if (me.ret == 0) {
             $('#btnLogin').hide();
         } else {
@@ -43,6 +56,37 @@ $(function () {
         }
     });
 });
+
+function showContactList() {
+    // 切换div显示
+    $('.current').removeClass('current');
+    $('#home').addClass('current');
+
+    $.when(dtdMe).then(function (me) {
+        if (me.ret != 0) return;
+
+        var gid = me.Id;
+        // 显示当前用户收藏的联系人
+        Nagu.SM.findByPO(Nagu.Contact.BelongsTo, gid).done(function (fss) {
+            if (bHidePublic) {
+                fss = $.grep(fss, function (fs, i) {
+                    return fs.AppId != Nagu.App.Public;
+                });
+            }
+            $('#contactList').statementList(fss, {
+                clearBefore: true,
+                renderItem: function (fs, li) {
+                    li.addClass('forward').appendMorpheme(fs.Subject);
+                    li.find('a').attr('href', '?id=' + fs.Subject.ConceptId)
+                        .unbind('click').click(function () {
+                            location = '?id=' + fs.Subject.ConceptId;
+                        });
+                }
+            });
+        });
+    });
+    
+}
 
 function showGroup(gid) {
     $('#home h1').appendConcept(gid, { container: $('#home h1') });
@@ -68,33 +112,38 @@ function showGroup(gid) {
 }
 
 function showContact(cid) {
+    // 切换div显示
+    $('.current').removeClass('current');
+    $('#contact').addClass('current');
 
     // 显示标题
     $('#contact h1').appendConcept(cid, { container: $('#contact h1') });
 
     // 设置“返回”按钮
     // 如果不存在主从关系，则设置返回“群组”首页。
-    if (contactsMS[cid] === undefined) {
-        $('#contact .back').unbind().attr('href', '#home');
-    } else {
-        $('#contact .back').unbind().attr('href', '#contact').click(function () {
-            showContact(contactsMS[cid]);
-        });
-    }
+    //if (contactsMS[cid] === undefined) {
+    //    $('#contact .back').unbind().attr('href', '#home');
+    //} else {
+    //    $('#contact .back').unbind().attr('href', '#contact').click(function () {
+    //        showContact(contactsMS[cid]);
+    //    });
+    //}
     
-    // 清除各个字段的值
+    // 清除各个固定字段的值
     var as = $('#itemList li a').size();
     $.each($('#itemList li a'),function(i,a){
         $(a).text('');
     });
 
     // 初始化显示样式
-    $('#contactFixedInfo').hide();
-    $('#contactFixedInfo').find('li').hide();
-    $('#contact_subGroup').hide();
-    $('#contact_subGroup').find('ul').empty();
+    //$('#contactFixedInfo').hide();
+    $('#itemList').find('li').hide();
+    $('#itemList').find('.nagu-loading').show();
+
     // 显示各个固定数据字段
-    Nagu.CM.pvsFromType(cid, Nagu.Contact.ContactClass).done(function (pvs) {
+    Nagu.CM.pvsFromType(cid, Nagu.Contact.Class).done(function (pvs) {
+
+        var hasData = false;
         $.each(pvs, function (i, pv) {
             var li = $('#property_' + pv.Key);
             if (li.size() == 0) return;
@@ -102,28 +151,34 @@ function showContact(cid) {
             // 如果存在属性值，则显示。
             if (pv.Value.length > 0) {
                 li.show();
-                $('#contactFixedInfo').show();
+                hasData = true;
                 li.find('a').text(pv.Value[0].Object.Value).attr('href','#item');
                 li.unbind().click(function () {
                     showItem(pv.Key, pv, cid);
                 });
-            } else {
-                li.find('a').append($('<font/>').attr('color', 'gray').text('点击添加'))
-                            .attr('href', '#addPv');
-                li.unbind().click(function () {
-                    //window.location = '#home';
-                });
+            //} else {
+            //    li.find('a').append($('<font/>').attr('color', 'gray').text('点击添加'))
+            //                .attr('href', '#addPv');
+            //    li.unbind().click(function () {
+            //        //window.location = '#home';
+            //    });
             }
         });
+        if (!hasData) {
+            $('#itemList .nagu-none').show();
+        }
+        $('#itemList').find('.nagu-loading').hide();
     });
 
     // 显示“下级联系人”列表
+    $('#contact_subGroup').hide();
+    $('#contact_subGroup').find('ul').empty();
     Nagu.SM.findByPO(Nagu.Contact.BelongsTo, cid, Nagu.MType.Concept).done(function (fss) {
         if (fss.length == 0) return;
 
         // 维护"联系人"主从关系表
         $.each(fss, function (i, fs) {
-            contactsMS[fss.Subject.ConceptId] = cid;
+            contactsMS[fs.Subject.ConceptId] = cid;
         });
         $('#contact_subGroup').show().statementList(fss, {
             clearBefore: true,
@@ -143,8 +198,8 @@ function showItem(propertyId, pv, cid) {
     $('#item h1').appendConcept(propertyId, { container: $('#item h1') });
     $('#item').appendConcept(cid, { container: $('#item .toolbar a') });
     var canDial = false;
-    if (propertyId == Nagu.Contact.Mobile
-        || propertyId == Nagu.Contact.OfficePhone)
+    if (propertyId == Nagu.Contact.CellPhoneNum
+        || propertyId == Nagu.Contact.OfficeNum)
         canDial = true;
 
     $('#itemValues').empty();
