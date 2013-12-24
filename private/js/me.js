e3846ad24bf88dd921376c03f7b16db412a9391f
na57 <na57@qq.com>
2013-12-24 11:56:42 +0800

    增加相册显示

--- private/js/me.js
+++ private/js/me.js
@@ -7,6 +7,7 @@ $(function () {
         if (me.ret == 0) { // 已登录
             afterNaguLogin(me);
             curUser = me.Id;
+            showPicList();
         } else { // 未登录
             naguLogout();
         }
@@ -16,3 +17,27 @@ $(function () {
     $('#btnClearStorage').btnCleanStorage();
 
 });
+
+
+
+function showPicList() {
+    var ul = $('.thumbnails').empty();
+    Nagu.CM.getPropertyValues(curUser, Nagu.User.HasPicture, {
+        appId: curUser
+    }).done(function (fss) {
+        $.each(fss, function (i, fs) {
+            if (fs.Object.Value) {
+                var url = fs.Object.Value; //+ '?' + randomInt();
+
+                // 如果图片来自微信，由于微信图片服务器无法直接连上，因此使用wrap中转。
+                //var regex = new RegExp("^http://mmsns.qpic.cn/mmsns/", "i");
+                //if (regex.test(url)) {
+                //    url = 'http://nagu.cc/func/wrap/?url=' + url;
+                //}
+                var img = B.img().attr('src', url);
+                var a = B.a().attr('target','_blank').attr('href', url).addClass('thumbnail').append(img);
+                var li = B.li().addClass('span2').append(a).appendTo(ul);
+            }
+        });
+    });
+}
